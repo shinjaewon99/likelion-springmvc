0fb54f25291933df6ed8d6699ac2366649498ebd
shinjaewon99 <tlswodnjs99@naver.com>
2023-06-07 20:21:32 +0900

    feat : 초기 설정 build

---
+++ src/main/java/com/example/likelion/LikelionApplication.java
@@ -0,0 +1,12 @@
+package com.example.likelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionApplication.class, args);
+    }
+}
