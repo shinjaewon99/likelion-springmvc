c6c2114d06b9592d4f3396d7c8376a9bbc2bd4e3
shinjaewon99 <tlswodnjs99@naver.com>
2023-06-20 18:00:32 +0900

    feat : RequestBody 컨트롤러 구현 - 요청헤더 로직 - JSON 응답 반환 로직

--- src/main/java/com/example/likelion/week10day1/BodyController.java
+++ src/main/java/com/example/likelion/week10day1/BodyController.java
@@ -1,8 +1,12 @@
 package com.example.likelion.week10day1;
 
+import com.example.likelion.week10day1.dto.ArticleComplexDto;
 import com.example.likelion.week10day1.dto.ArticleDto;
 import com.example.likelion.week10day1.dto.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,10 +22,50 @@ public class BodyController {
     public @ResponseBody ResponseDto body(
             @RequestBody ArticleDto requestDto
     ) {
-
+        log.info(requestDto.toString());
         ResponseDto response = new ResponseDto();
         response.setStatus(200);
         response.setMessage("success");
         return response;
+    }
+
+    @PostMapping("/body-1")
+    public @ResponseBody ResponseDto body1(
+            @RequestBody ArticleComplexDto requestDto
+    ) {
+        log.info(requestDto.toString());
+        ResponseDto response = new ResponseDto();
+        response.setStatus(200);
+        response.setMessage("success");
+        return response;
+    }
+
+    // @ResponseBody는 요청의 HTTP Body만 설정
+    // 헤더를 추가하거나
+    // Status code를 고르고 싶을떄
+    // ResponseEntity<T>
+    @PostMapping("/entity")
+    public ResponseEntity<ResponseDto> entity(
+            @RequestBody ArticleDto requestDto
+    ) {
+        log.info(requestDto.toString());
+        ResponseDto response = new ResponseDto();
+        response.setStatus(200);
+        response.setMessage("success");
+        ResponseEntity<ResponseDto> responseEntity = new ResponseEntity<>(response, HttpStatus.OK);
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("x-likelion-custom", "hello");
+                
+//        return new ResponseEntity<>(
+//                response, headers, HttpStatus.ACCEPTED
+//        );
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("x-likelion-one", "1")
+                .headers(headers)
+                .body(response);
     }
 }
